fab5e613394e265bbb7c12a2f21252f778bb7d86
Ahmed Ayman Saad <34175435+AhmedAymanSaad@users.noreply.github.com>
2023-04-22 22:21:03 +0200

    changed port

--- Backend/NextReads/index.js
+++ Backend/NextReads/index.js
@@ -13,7 +13,7 @@ mongoose.connect('mongodb+srv://AhmedAyman:1102@nextreads.he0ja7f.mongodb.net/ne
     .then(() => console.log('connected to MongoDB'))
     .catch(err => console.error('could not connect to MongoDB', err));
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 80;
 app.listen(port, () => console.log(`listening on port ${port}`));
 //404 not found
 // 400 bad request
