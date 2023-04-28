bef90cc8696d5cd1744f44d86fcdc4a520c874a1
Ahmed Ayman Saad <34175435+AhmedAymanSaad@users.noreply.github.com>
2023-04-28 17:44:19 +0300

    changed deployment settings

--- Recommender/RecommenderGatewayApp/app.py
+++ Recommender/RecommenderGatewayApp/app.py
@@ -171,4 +171,4 @@ def index():
     return 'Coming Soon!'
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 80))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port, threaded=True, use_reloader=False)
