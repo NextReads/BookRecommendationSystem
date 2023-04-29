b440365b71d660779c4639e2884587e8584aaa5c
ArwaShamardal <76781020+ArwaShamardal@users.noreply.github.com>
2023-04-29 12:03:49 +0200

    Updated needed files

--- Recommender/Utils/constants.py
+++ Recommender/Utils/constants.py
@@ -1,6 +1,7 @@
 BOOKS_DF_PATH = 'dataset/goodreads_books_shrink.csv'
 RATINGS_DF_PATH = 'dataset/goodreads_reviews_shrink.csv'
 GENRES_DF_PATH = 'dataset/goodreads_genres_shrink.csv'
+RATINGS_DS_PATH = 'dataset/ratings.csv'
 K_FEATURES = 20
 CB_TOP_N_GENRES = 3
 CB_TOP_N_BOOKS = 20
