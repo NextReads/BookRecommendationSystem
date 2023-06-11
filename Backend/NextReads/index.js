69095c2f5df09107d9ad8368ab5cce896d630b47
Ahmed Ayman Saad <34175435+AhmedAymanSaad@users.noreply.github.com>
2023-06-11 10:13:24 +0300

    Update index.js

--- Backend/NextReads/index.js
+++ Backend/NextReads/index.js
@@ -9,7 +9,7 @@ if (!process.env['JWTPRIVATEKEY']) {
   }
 
 
-mongoose.connect('mongodb+srv://brutefoursteam:1234@nextreads.phkbn.mongodb.net//test')
+mongoose.connect('mongodb+srv://brutefoursteam:1234@nextreads.phkbn.mongodb.net/test')
     .then(() => console.log('connected to MongoDB'))
     .catch(err => console.error('could not connect to MongoDB', err));
 
